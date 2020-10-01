48045bc97ba56cc95540b094511c7fd524401d49
Sean McPherson <git@seanmcp.com>
2020-09-30 21:03:57 -0400

    init

---
+++ main.js
@@ -0,0 +1,125 @@
+(() => {
+  const { el, formatDuration, formatTimerTime, getStart, Store } = window._app;
+
+  const startTime = getStart();
+
+  if (startTime) {
+    el.start.hidden = true;
+
+    function writeTimer() {
+      el.timer.textContent = formatTimerTime(new Date().getTime() - startTime);
+    }
+
+    writeTimer();
+
+    window._app.timerInterval = setInterval(writeTimer, 1000);
+  } else {
+    el.stop.hidden = true;
+    clearInterval(window._app.timerInterval);
+  }
+
+  el.start.addEventListener("click", (event) => {
+    event.target.href += "?start=" + new Date().getTime();
+  });
+
+  el.stop.addEventListener("click", () => {
+    const stop = new Date().getTime();
+    const start = getStart();
+
+    Store.setData([
+      ...Store.getData(),
+      {
+        start,
+        stop,
+        rating: 1,
+      },
+    ]);
+  });
+
+  function changeRating(stop, rating) {
+    const data = Store.getData();
+    const next = data.map((entry) => {
+      if (entry.stop == stop) {
+        return {
+          ...entry,
+          rating,
+        };
+      }
+      return entry;
+    });
+
+    Store.setData(next);
+  }
+
+  function deleteEntry(stop) {
+    const data = Store.getData();
+    const next = data.filter((entry) => entry.stop != stop);
+
+    Store.setData(next);
+  }
+
+  function renderRecord() {
+    const reverseChron = Store.getData().reverse();
+    el.record.innerHTML = "";
+
+    reverseChron.forEach((entry, i) => {
+      const last = reverseChron[i + 1];
+      const start = parseInt(entry.start);
+      const stop = parseInt(entry.stop);
+
+      const row = document.createElement("tr");
+      el.record.appendChild(row);
+
+      const ratingCell = document.createElement("td");
+      const select = document.createElement("select");
+      select.setAttribute("aria-label", "rating");
+      select.dataset.rating = entry.rating;
+      select.addEventListener("change", (event) => {
+        select.dataset.rating = event.target.value;
+        changeRating(stop, event.target.value);
+      });
+
+      for (let i = 1; i <= 3; i++) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = "⚡️".repeat(i);
+        option.selected = i == entry.rating;
+        select.appendChild(option);
+      }
+
+      ratingCell.appendChild(select);
+      row.appendChild(ratingCell);
+
+      const startCell = document.createElement("td");
+      const startDate = new Date(start);
+      const h = startDate.getHours();
+      startCell.textContent = `${h > 12 ? h - 12 : 0}:${String(
+        startDate.getMinutes()
+      ).padStart(2, "0")}${h > 12 ? "pm" : "am"}`;
+      row.appendChild(startCell);
+
+      const lengthCell = document.createElement("td");
+      lengthCell.textContent = formatDuration(stop - start);
+      row.appendChild(lengthCell);
+
+      const frequencyCell = document.createElement("td");
+      frequencyCell.textContent = last
+        ? formatDuration(start - parseInt(last.stop))
+        : "First";
+      row.appendChild(frequencyCell);
+
+      const deleteCell = document.createElement("td");
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "🗑";
+      deleteButton.setAttribute('aria-label', 'Delete')
+      deleteButton.addEventListener("click", () => {
+        deleteEntry(stop);
+        renderRecord();
+      });
+      deleteCell.appendChild(deleteButton);
+      row.appendChild(deleteCell);
+    });
+  }
+
+  renderRecord();
+})();
---
+++ setup.js
@@ -0,0 +1,60 @@
+(() => {
+  const Store = {
+    KEY: "contraction",
+    getData() {
+      return JSON.parse(localStorage.getItem(this.KEY)) || [];
+    },
+    setData(data) {
+      localStorage.setItem(this.KEY, JSON.stringify(data));
+    },
+  };
+
+  function getTimeParts(ms) {
+    let h,
+      m,
+      s;
+    h = Math.floor(ms / 1000 / 60 / 60);
+    m = Math.floor((ms / 1000 / 60 / 60 - h) * 60);
+    s = Math.floor(((ms / 1000 / 60 / 60 - h) * 60 - m) * 60);
+
+    return { h, m, s }
+  }
+
+  function formatDuration(ms) {
+    const { h, m, s } = getTimeParts(ms)
+
+    if (h) return `${h}h ${m}m`
+    if (m) return `${m}m ${s}s`
+    return `${s}s`
+  }
+
+  function formatTimerTime(ms) {
+    const { h, m, s } = getTimeParts(ms)
+    let output = ''
+
+    if (h) output += h + ":";
+    output += String(m).padStart(2, "0");
+    output += ":";
+    output += String(s).padStart(2, "0");
+
+    return output;
+  }
+
+  function getStart() {
+    return new URLSearchParams(window.location.search).get("start");
+  }
+
+  window._app = {
+    el: {
+      record: document.getElementById('record'),
+      start: document.getElementById('start'),
+      stop: document.getElementById('stop'),
+      timer: document.getElementById('timer')
+    },
+    formatDuration,
+    formatTimerTime,
+    getStart,
+    Store,
+    timerInterval: null,
+  };
+})();
